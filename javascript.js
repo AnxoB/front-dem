f5eacbb70206efd8dd47d5be5ba5b64604824727
dam1 - Búa Alonso <A22AnxoBA@sanclemente.local>
2025-01-15 14:34:18 +0100

    Version 0.1

---
+++ javascript.js
@@ -0,0 +1,22 @@
+
+function changeBG(){
+    let paragraph = document.getElementById("para");
+    paragraph.style.backgroundColor = "red";
+}
+
+function changeBG2(){
+    let paragraph = document.getElementById("para");
+    paragraph.style.backgroundColor = "blue";
+}
+
+function changeText(){
+    let paragraph = document.getElementById("para");
+    paragraph.innerText="Text changed";
+}
+
+function seeInfo(){
+    let paragraph2 = document.getElementById("para2")
+    document.write(paragraph2.textContent) //Muestra el texto del parágrafo sin el strong
+    document.write(paragraph2.innerHTML) //Muestra el texto con el strong
+}
+
